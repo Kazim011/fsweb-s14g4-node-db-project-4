e868773eeda49da0d4d12c7f518a406de62057f3
kazım <kazimnergiz123@gmail.com>
2023-03-03 00:10:47 +0300

    S14G4 tamamlandı

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const tarifRouter = require("./tarifler/tarifler-router");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/tarifler", tarifRouter);
+
+module.exports = server;
